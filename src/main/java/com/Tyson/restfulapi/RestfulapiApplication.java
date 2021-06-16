9582374d235c080387bc55e980688c4d6ad584ea
Tyson Code Cademy <Tyson3rd@Michelles-MacBook-Air.local>
2021-06-16 08:58:18 -0500

    Last commit

--- src/main/java/com/Tyson/restfulapi/RestfulapiApplication.java
+++ src/main/java/com/Tyson/restfulapi/RestfulapiApplication.java
@@ -1,13 +1,13 @@
 package com.Tyson.restfulapi;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestfulapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulapiApplication.class, args);
+    }
 
 }
